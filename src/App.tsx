b4976778ca07d3caa00dcdb672251dc63736528d
Kaguenoakuma <shadowfocker666@gmail.com>
2025-01-15 19:41:48 -0600

    Inicio del proyecto Buscador de Clima

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css";
+
+function App() {
+  
+  return (
+    <>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+      <div className={styles.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
+    </>
+  )
+}
+
+export default App
